d30f64da14cbed55ebef24ecbfdf7b0540dbd6c7
munyagisaka <168621581+munyagisaka@users.noreply.github.com>
2024-10-27 10:30:23 +0200

    Add files via upload

---
+++ Runniing.py
@@ -0,0 +1,32 @@
+kilometres =12.25
+miles = 7.28
+miles_to_kilometres = 12.25*1.61
+kilometres_to_miles = 7.28/1.61
+print()
+print(miles,"miles is",round(miles_to_kilometres,2), "kilometres")
+print(kilometres,"kilometres is", round(kilometres_to_miles,2), "miles")
+
+degree_celsius = 87
+kelvin = 300
+
+degree_celsius_to_kelvin = 87+273
+kelvin_to_degree_celsius = 300-273
+print()
+print(kelvin,"Kelvins is", kelvin_to_degree_celsius, "Degree Celsius")
+print()
+print(degree_celsius, "Degree Celsius is", degree_celsius_to_kelvin,"Kelvins")
+
+
+
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+number3= "No larger number"
+ 
+# Choose the larger number
+if number1 > number2: larger_number = number1
+elif number1:= number2: larger_number = number3
+else: larger_number =  number2
+ 
+# Print the result
+print( larger_number)
+ 
